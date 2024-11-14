2399e6a8a3c2f53eb96cb07477f5a45d84839528
yuliia <yuliia.nad@g-experts.net>
2024-11-14 09:31:09 +0100

    Auth for User and Admin role, where user accesses only the endpoint for getting insurance details by customerId

--- src/main/java/com/insurancesimulator/config/SecurityConfiguration.java
+++ src/main/java/com/insurancesimulator/config/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.insurancesimulator.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,27 +18,48 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Value("${creds.user.name}")
+    String userName;
+
+    @Value("${creds.user.password}")
+    String userPass;
+
+    @Value("${creds.admin.name}")
+    String adminName;
+
+    @Value("${creds.admin.password}")
+    String adminPass;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
-                    .requestMatchers("/{customerId}").hasRole("USER")
+                    //TODO assign the customerId to specific user and
+                    // let him access only his own insDetails
+                    .requestMatchers("/api/insurance/{customerId}").hasAnyRole("USER", "ADMIN")
                     .anyRequest().hasRole("ADMIN")
             )
-            .formLogin(withDefaults())
+            .formLogin(formLogin ->
+                formLogin
+                    .defaultSuccessUrl("/api/insurance", true)
+                    .permitAll()
+            )
+
+            .httpBasic(withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
             .logout(LogoutConfigurer::permitAll);
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user")
-            .password("{noop}user")
+        UserDetails user = User.withUsername(userName)
+            .password(userPass)
             .roles("USER")
             .build();
 
-        UserDetails admin = User.withUsername("admin")
-            .password("{noop}admin")
+        UserDetails admin = User.withUsername(adminName)
+            .password(adminPass)
             .roles("ADMIN")
             .build();
         return new InMemoryUserDetailsManager(user, admin);
